b2407e1257cb22170008e22d14d95aba4f8a20ec
Maciej Olko <maciej.olko@affirm.com>
2023-08-08 22:49:18 +0200

    Fetch translations from Weblate

--- manage_translation.py
+++ manage_translation.py
@@ -1,26 +1,12 @@
-#!/usr/bin/env python
-#
-# This python file contains utility scripts to manage Python docs Polish translation.
-# It has to be run inside the python-docs-pl git root directory.
-#
-# Inspired by django-docs-translations script by claudep.
-#
-# The following commands are available:
-#
-# * fetch: fetch translations from transifex.com and strip source lines from the
-#          files.
-# * recreate_tx_config: recreate configuration for all resources.
-
-from argparse import ArgumentParser
+"""
+Utility scripts for Python docs Polish translation.
+It has to be run inside the python-docs-pl git root directory.
+"""
 import os
-from contextlib import chdir
 from dataclasses import dataclass
 from difflib import SequenceMatcher
 from itertools import combinations
 from pathlib import Path
-from subprocess import call
-import sys
-from tempfile import TemporaryDirectory
 from typing import Self, Generator, Iterable
 from warnings import warn
 
@@ -28,67 +14,7 @@ from polib import pofile
 from transifex.api import transifex_api
 
 LANGUAGE = 'pl'
-
-
-def fetch():
-    """
-    Fetch translations from Transifex, remove source lines.
-    """
-    if (code := call("tx --version", shell=True)) != 0:
-        sys.stderr.write("The Transifex client app is required.\n")
-        exit(code)
-    lang = LANGUAGE
-    _call(f'tx pull -l {lang} --minimum-perc=1 --force --skip')
-    for file in Path().rglob('*.po'):
-        _call(f'msgcat --no-location -o {file} {file}')
-
-
-def _call(command: str):
-    if (return_code := call(command, shell=True)) != 0:
-        exit(return_code)
-
-
 PROJECT_SLUG = 'python-newest'
-VERSION = '3.12'
-
-
-def recreate_tx_config():
-    """
-    Regenerate Transifex client config for all resources.
-    """
-    with TemporaryDirectory() as directory:
-        with chdir(directory):
-            _clone_cpython_repo(VERSION)
-            _build_gettext()
-            with chdir(Path(directory) / 'cpython/Doc/locales'):
-                _create_txconfig()
-                _update_txconfig_resources()
-                with open('.tx/config', 'r') as file:
-                    contents = file.read()
-        contents = contents.replace('./<lang>/LC_MESSAGES/', '')
-        with open('.tx/config', 'w') as file:
-            file.write(contents)
-
-
-def _clone_cpython_repo(version: str):
-    _call(f'git clone -b {version} --single-branch https://github.com/python/cpython.git --depth 1')
-
-
-def _build_gettext():
-    _call(
-        "make -C cpython/Doc/ ALLSPHINXOPTS='-E -b gettext -D gettext_compact=0 -d build/.doctrees . locales/pot' build"
-    )
-
-
-def _create_txconfig():
-    _call('sphinx-intl create-txconfig')
-
-
-def _update_txconfig_resources():
-    _call(
-        f'sphinx-intl update-txconfig-resources --transifex-organization-name python-doc '
-        f'--transifex-project-name={PROJECT_SLUG} --locale-dir . --pot-dir pot'
-    )
 
 
 @dataclass
@@ -168,13 +94,3 @@ def _check_for_aliases(translators) -> None:
 def language_switcher(entry: ResourceLanguageStatistics) -> bool:
     language_switcher_resources_prefixes = ('bugs', 'tutorial', 'library--functions')
     return any(entry.name.startswith(prefix) for prefix in language_switcher_resources_prefixes)
-
-
-if __name__ == "__main__":
-    RUNNABLE_SCRIPTS = ('fetch', 'recreate_tx_config')
-
-    parser = ArgumentParser()
-    parser.add_argument('cmd', choices=RUNNABLE_SCRIPTS)
-    options = parser.parse_args()
-
-    eval(options.cmd)()
