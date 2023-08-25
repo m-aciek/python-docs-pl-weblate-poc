05879dfc031da70ed58609c88b68b25893966078
Maciej Olko <maciej.olko@affirm.com>
2023-08-26 00:22:51 +0200

    Fetch translations from Weblate (#1)
    * Fetch translations from Weblate
    
    * Revert removing transifex-python install
    
    * Use Weblate for statistics
    
    * Checkout current branch
    
    * Install tqdm
    
    * Add secret for Weblate
    
    * Fix secret name for Weblate
    
    * Fix logging to stdout with tqdm
    
    * Fix .update-translation.py argument value

--- README.md
+++ README.md
@@ -15,9 +15,9 @@ f'''![build](https://github.com/python/python-docs-pl/workflows/.github/workflow
 ![{translators} tłumaczy](https://img.shields.io/badge/tłumaczy-{translators}-0.svg)''')
 ]]] -->
 ![build](https://github.com/python/python-docs-pl/workflows/.github/workflows/update-lint-and-build.yml/badge.svg)
-![46.96% przełącznika języków](https://img.shields.io/badge/przełącznik_języków-46.96%25-0.svg)
-![postęp tłumaczenia całości dokumentacji](https://img.shields.io/badge/całość-2.79%25-0.svg)
-![19 tłumaczy](https://img.shields.io/badge/tłumaczy-19-0.svg)
+![48.30% przełącznika języków](https://img.shields.io/badge/przełącznik_języków-48.30%25-0.svg)
+![postęp tłumaczenia całości dokumentacji](https://img.shields.io/badge/całość-3.45%25-0.svg)
+![18 tłumaczy](https://img.shields.io/badge/tłumaczy-18-0.svg)
 <!-- [[[end]]] -->
 
 Jeśli znalazłeś(-aś) błąd lub masz sugestię,
--- manage_translation.py
+++ manage_translation.py
@@ -1,94 +1,22 @@
-#!/usr/bin/env python
-#
-# This python file contains utility scripts to manage Python docs Polish translation.
-# It has to be run inside the python-docs-pl git root directory.
-#
-# Inspired by django-docs-translations script by claudep.
-#
-# The following commands are available:
-#
-# * fetch: fetch translations from transifex.com and strip source lines from the
-#          files.
-# * recreate_tx_config: recreate configuration for all resources.
-
-from argparse import ArgumentParser
-import os
-from contextlib import chdir
+"""
+Utility scripts for Python docs Polish translation.
+It has to be run inside the python-docs-pl git root directory.
+"""
 from dataclasses import dataclass
 from difflib import SequenceMatcher
 from itertools import combinations
+from logging import warning, basicConfig
+from os import getenv
 from pathlib import Path
-from subprocess import call
-import sys
-from tempfile import TemporaryDirectory
-from typing import Self, Generator, Iterable
+from typing import Generator, Iterable, Self
 from warnings import warn
 
 from polib import pofile
-from transifex.api import transifex_api
+from tqdm import tqdm
+from wlc import Component, LanguageStats, Project, Statistics, TranslationStatistics, Weblate, WeblateException
 
 LANGUAGE = 'pl'
-
-
-def fetch():
-    """
-    Fetch translations from Transifex, remove source lines.
-    """
-    if (code := call("tx --version", shell=True)) != 0:
-        sys.stderr.write("The Transifex client app is required.\n")
-        exit(code)
-    lang = LANGUAGE
-    _call(f'tx pull -l {lang} --minimum-perc=1 --force --skip')
-    for file in Path().rglob('*.po'):
-        _call(f'msgcat --no-location -o {file} {file}')
-
-
-def _call(command: str):
-    if (return_code := call(command, shell=True)) != 0:
-        exit(return_code)
-
-
 PROJECT_SLUG = 'python-newest'
-VERSION = '3.12'
-
-
-def recreate_tx_config():
-    """
-    Regenerate Transifex client config for all resources.
-    """
-    with TemporaryDirectory() as directory:
-        with chdir(directory):
-            _clone_cpython_repo(VERSION)
-            _build_gettext()
-            with chdir(Path(directory) / 'cpython/Doc/locales'):
-                _create_txconfig()
-                _update_txconfig_resources()
-                with open('.tx/config', 'r') as file:
-                    contents = file.read()
-        contents = contents.replace('./<lang>/LC_MESSAGES/', '')
-        with open('.tx/config', 'w') as file:
-            file.write(contents)
-
-
-def _clone_cpython_repo(version: str):
-    _call(f'git clone -b {version} --single-branch https://github.com/python/cpython.git --depth 1')
-
-
-def _build_gettext():
-    _call(
-        "make -C cpython/Doc/ ALLSPHINXOPTS='-E -b gettext -D gettext_compact=0 -d build/.doctrees . locales/pot' build"
-    )
-
-
-def _create_txconfig():
-    _call('sphinx-intl create-txconfig')
-
-
-def _update_txconfig_resources():
-    _call(
-        f'sphinx-intl update-txconfig-resources --transifex-organization-name python-doc '
-        f'--transifex-project-name={PROJECT_SLUG} --locale-dir . --pot-dir pot'
-    )
 
 
 @dataclass
@@ -98,38 +26,57 @@ class ResourceLanguageStatistics:
     translated_words: int
     total_strings: int
     translated_strings: int
+    exists: bool
 
     @classmethod
-    def from_api_entry(cls, data: transifex_api.ResourceLanguageStats) -> Self:
+    def from_wlc(cls, component_name: str, stats: TranslationStatistics) -> Self:
         return cls(
-            name=data.id.removeprefix(f'o:python-doc:p:{PROJECT_SLUG}:r:').removesuffix(f':l:{LANGUAGE}'),
-            total_words=data.attributes['total_words'],
-            translated_words=data.attributes['translated_words'],
-            total_strings=data.attributes['total_strings'],
-            translated_strings=data.attributes['translated_strings'],
+            name=component_name,
+            total_words=stats.total_words,
+            translated_words=stats.translated_words,
+            total_strings=stats.total,
+            translated_strings=stats.translated,
+            exists=True,
+        )
+
+    @classmethod
+    def empty_from_any_wlc(cls, component_name: str, stats: TranslationStatistics) -> Self:
+        return cls(
+            name=component_name,
+            total_words=stats.total_words,
+            translated_words=0,
+            total_strings=stats.total,
+            translated_strings=0,
+            exists=False,
         )
 
 
-def _get_tx_token() -> str:
-    if os.path.exists('.tx/api-key'):
-        with open('.tx/api-key') as f:
-            transifex_api_key = f.read()
-    else:
-        transifex_api_key = os.getenv('TX_TOKEN', '')
-    return transifex_api_key
+def _get_resources(language: str, weblate_key: str) -> Generator[ResourceLanguageStatistics, None, None]:
+    weblate = Weblate(weblate_key, 'https://hosted.weblate.org/api/')
+    project = Project(weblate, 'https://hosted.weblate.org/api/projects/python-docs/')
+    for component in tqdm(project.list()):
+        try:
+            yield ResourceLanguageStatistics.from_wlc(
+                component.name,
+                TranslationStatistics(
+                    weblate,
+                    f'https://hosted.weblate.org/api/translations/python-docs/{component.slug}/{language}/statistics/'
+                )
+            )
+        except WeblateException as exc:
+            if str(exc) == 'Object not found on the server (maybe operation is not supported on the server)':
+                yield ResourceLanguageStatistics.empty_from_any_wlc(component.name, next(component.statistics()))
+            else:
+                raise
 
 
-def _get_resources() -> list[transifex_api.Resource]:
-    transifex_api.setup(auth=_get_tx_token())
-    return transifex_api.Resource.filter(project=f'o:python-doc:p:{PROJECT_SLUG}').all()
-
-
-def get_resource_language_stats() -> list[ResourceLanguageStatistics]:
-    transifex_api.setup(auth=_get_tx_token())
-    resources = transifex_api.ResourceLanguageStats.filter(
-        project=f'o:python-doc:p:{PROJECT_SLUG}', language=f'l:{LANGUAGE}'
-    ).all()
-    return [ResourceLanguageStatistics.from_api_entry(entry) for entry in resources]
+def get_resource_language_stats(language: str = None, weblate_key: str = None) -> list[ResourceLanguageStatistics]:
+    if not language:
+        language = LANGUAGE
+    if not weblate_key:
+        if not (weblate_key := getenv('KEY')):
+            warning('Not authenticated, you will be heavy throttled')
+    return list(_get_resources(language, weblate_key))
 
 
 def progress_from_resources(resources: Iterable[ResourceLanguageStatistics]) -> float:
@@ -166,15 +113,8 @@ def _check_for_aliases(translators) -> None:
 
 
 def language_switcher(entry: ResourceLanguageStatistics) -> bool:
-    language_switcher_resources_prefixes = ('bugs', 'tutorial', 'library--functions')
+    language_switcher_resources_prefixes = ('bugs', 'tutorial', 'library/functions')
     return any(entry.name.startswith(prefix) for prefix in language_switcher_resources_prefixes)
 
 
-if __name__ == "__main__":
-    RUNNABLE_SCRIPTS = ('fetch', 'recreate_tx_config')
-
-    parser = ArgumentParser()
-    parser.add_argument('cmd', choices=RUNNABLE_SCRIPTS)
-    options = parser.parse_args()
-
-    eval(options.cmd)()
+basicConfig(level='INFO')
